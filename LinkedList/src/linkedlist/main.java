f1c4a161cb8e16197b76e9a8cef9b96de7e144db
Muhammad Owais <owaissyed65@gmail.com>
2023-08-10 22:44:23 +0500

    linkedlist

---
+++ LinkedList/src/linkedlist/main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package linkedlist;
+
+/**
+ *
+ * @author GC
+ */
+public class main {
+
+    public static void main(String[] args) {
+        LinkedList link = new LinkedList();
+        link.insert(4);
+        link.insert(4);
+        System.out.println(link);
+    }
+}
